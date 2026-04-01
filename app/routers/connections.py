16079f670ad05fcdf2f14dde276ca4f278b449d4
danielsong <jsong32@uw.edu>
2026-03-31 19:02:22 -0700

    get and delete connections

--- app/routers/connections.py
+++ app/routers/connections.py
@@ -86,3 +86,44 @@ def decline_connection(current_user: Annotated[str, Depends(auth.get_current_use
         status=connection.status,
         created_at=connection.created_at
     )
+
+
+@router.get("/", response_model=list[schemas.ConnectionResponse])
+def get_connections(current_user: Annotated[str, Depends(auth.get_current_user)], db: Annotated[Session, Depends(get_db)], status: str="pending"):
+    user = db.query(models.User).filter(models.User.username == current_user).first()
+    connections = db.query(models.Connection).filter(
+        models.Connection.status == status,
+        or_(
+            models.Connection.requester_id == user.id,
+            models.Connection.receiver_id == user.id
+        )
+    ).all()
+    my_connections = []
+    for connection in connections:
+        requester = db.query(models.User).filter(models.User.id == connection.requester_id).first()
+        receiver = db.query(models.User).filter(models.User.id == connection.receiver_id).first()
+        my_connections.append(schemas.ConnectionResponse(
+            requester_username=requester.username,
+            receiver_username=receiver.username,
+            status=connection.status,
+            created_at=connection.created_at
+        ))
+    return my_connections
+
+
+@router.delete("/{username}", status_code=204)
+def delete_connections(current_user: Annotated[str, Depends(auth.get_current_user)], db: Annotated[Session, Depends(get_db)], username: str):
+    user = db.query(models.User).filter(models.User.username == current_user).first()
+    other_user = db.query(models.User).filter(models.User.username == username).first()
+    if not other_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    connection = db.query(models.Connection).filter(
+        or_((models.Connection.requester_id == user.id) & (models.Connection.receiver_id == other_user.id),
+            (models.Connection.requester_id == other_user.id) & (models.Connection.receiver_id == user.id))
+    ).first()
+    if not connection:
+        raise HTTPException(status_code=404, detail="Connection not found")
+    
+    db.delete(connection)
+    db.commit()
